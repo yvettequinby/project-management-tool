ce5311114af8affb086c76ed440ac1a0872594aa
Yvette Quinby <yvette.quinby@protonmail.com>
2019-01-20 10:20:03 +0100

    initial commit

---
+++ project-management-tool/src/main/java/com/javafreelancedeveloper/projectmanagementtool/ProjectManagementToolApplication.java
@@ -0,0 +1,14 @@
+package com.javafreelancedeveloper.projectmanagementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagementToolApplication.class, args);
+    }
+
+}
+
