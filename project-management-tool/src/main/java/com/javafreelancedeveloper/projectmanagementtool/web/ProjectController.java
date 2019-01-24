4379e67044840136270772aebb9894ff70fd9c06
Yvette Quinby <yvette.quinby@protonmail.com>
2019-01-24 14:40:10 +0100

    cross origin for react client

--- project-management-tool/src/main/java/com/javafreelancedeveloper/projectmanagementtool/web/ProjectController.java
+++ project-management-tool/src/main/java/com/javafreelancedeveloper/projectmanagementtool/web/ProjectController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("api/project")
 @AllArgsConstructor
+@CrossOrigin
 public class ProjectController extends BaseController {
 
     private final ProjectService projectService;
