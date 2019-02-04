d0661316bba2e1150215eb614f9860d5bf8215ed
Yvette Quinby <yvette.quinby@protonmail.com>
2019-02-04 15:41:44 +0100

    Start of front-end user management and security

--- project-management-tool-react-client/src/App.js
+++ project-management-tool-react-client/src/App.js
@@ -11,6 +11,9 @@ import store from "./store";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
 import AddProjectTask from "./components/ProjectBoard/AddProjectTask";
 import UpdateProjectTask from "./components/ProjectBoard/UpdateProjectTask";
+import Landing from "./components/Layout/Landing";
+import Register from "./components/UserManagement/Register";
+import Login from "./components/UserManagement/Login";
 
 class App extends Component {
   render() {
@@ -19,6 +22,17 @@ class App extends Component {
         <Router>
           <div className="App">
             <Header />
+
+            {
+              // Public Routes
+            }
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+
+            {
+              // Private Routes
+            }
             <Route exact path="/Dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
             <Route
---
+++ project-management-tool-react-client/src/components/UserManagement/Register.js
@@ -0,0 +1,129 @@
+import React, { Component } from "react";
+import { createNewUser } from "../../actions/securityActions";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
+import FieldErrors from "../Layout/FieldErrors";
+
+class Register extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: "",
+      fullName: "",
+      password: "",
+      confirmPassword: "",
+      errors: {}
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  // life cycle hooks
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const newUser = {
+      username: this.state.username,
+      fullName: this.state.fullName,
+      password: this.state.password,
+      confirmPassword: this.state.confirmPassword
+    };
+
+    this.props.createNewUser(newUser, this.props.history);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  render() {
+    const { errors } = this.state;
+    return (
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">Create your Account</p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    name="fullName"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.fullName
+                    })}
+                    value={this.state.fullName}
+                    onChange={this.onChange}
+                  />
+                  <FieldErrors msgs={errors.fullName} />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    placeholder="Email Address"
+                    name="username"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.username
+                    })}
+                    value={this.state.username}
+                    onChange={this.onChange}
+                  />
+                  <FieldErrors msgs={errors.username} />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password
+                    })}
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                  <FieldErrors msgs={errors.password} />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    name="confirmPassword"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.confirmPassword
+                    })}
+                    value={this.state.confirmPassword}
+                    onChange={this.onChange}
+                  />
+                  <FieldErrors msgs={errors.confirmPassword} />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+Register.propTypes = {
+  createNewUser: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { createNewUser }
+)(Register);
